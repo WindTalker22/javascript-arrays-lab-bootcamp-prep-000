c7f726b879276801208d09de94b3e447c362d917
Stephen Saciolo <ssaciolo@gmail.com>
2019-06-20 21:29:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(name){
-  return kittens.concat([kittens] + name);
-}
